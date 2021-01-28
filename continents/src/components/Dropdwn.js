69f6761953516c2ea350a0d76f455bbc3dfebb33
Stanislav Palchyk <stanislav.palchyk@helsinki.fi>
2021-01-28 20:51:04 +0200

    using bootstrap instead of semantic and trying variaty of dropdown types

--- continents/src/components/Dropdwn.js
+++ continents/src/components/Dropdwn.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { /*useState*/ } from 'react';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import Button from 'react-bootstrap/Button'
@@ -13,58 +13,68 @@ let continents = [
 
 const Dropdwn = () => {
     return (<>
-
-        <DropdownButton id="dropdown-basic-button" title="Countries">
-            {continents.map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <div align="center">
             <DropdownButton id="dropdown-basic-button" title="Countries">
                 {continents.map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </DropdownButton>
-        </DropdownButton>
-
-
-
-        <Dropdown >
-            <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
-            <Dropdown.Menu className="super-colors">
-                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-1">z</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">z action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">z else</Dropdown.Item>
                 <DropdownButton id="dropdown-basic-button" title="Countries">
                     {continents.map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+
+                </DropdownButton>
+            </DropdownButton>
+
+
+
+            <Dropdown >
+                <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
+                <Dropdown.Menu className="super-colors">
+                    <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
                     <DropdownButton id="dropdown-basic-button" title="Countries">
                         {continents.map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}
                         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
                         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        <DropdownButton id="dropdown-basic-button" title="Countries">
+                            {continents.map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </DropdownButton>
                     </DropdownButton>
-                </DropdownButton>
-                <Dropdown.Divider />
-                <Dropdown >
-                    <Button variant="info">mix it up style-wise</Button>
-                    <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
-                    <Dropdown.Menu className="super-colors">
-                        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                        <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                        <Dropdown.Item eventKey="3" active>
-                            Active Item
+                    <Dropdown.Divider />
+                    <Dropdown >
+                        <Button variant="info">mix it up style-wise</Button>
+                        <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
+                        <Dropdown.Menu className="super-colors">
+                            <Dropdown.Item eventKey="1" active>Action</Dropdown.Item>
+                            <Dropdown.Item eventKey="2" >Another action</Dropdown.Item>
+                            <Dropdown.Item eventKey="3" active>
+                                Active Item sdfsdlfhskjdhfkj
       </Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </Dropdown.Menu>
-        </Dropdown>{' '}
+                            <Dropdown.Divider />
+                            <Dropdown >
+                                <Button variant="info">mix it up style-wise</Button>
+                                <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
+                                <Dropdown.Menu className="super-colors">
+                                    <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                                    <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                                    <Dropdown.Item eventKey="3" active>
+                                        Active Item
+      </Dropdown.Item>
+                                    <Dropdown.Divider />
+                                    <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Dropdown.Menu>
+            </Dropdown>{' '}
 
 
-    </>);
+        </div></>);
 }
 
 export default Dropdwn;
