03fc26f7d32426dec7954a72696b931123f7308e
Stanislav Palchyk <stanislav.palchyk@helsinki.fi>
2021-01-29 16:46:57 +0200

    trying map and filter in dropdowns

--- continents/src/components/Dropdwn.js
+++ continents/src/components/Dropdwn.js
@@ -13,21 +13,18 @@ let continents = [
 
 const Dropdwn = () => {
     return (<>
-        <div align="center">
-            <DropdownButton id="dropdown-basic-button" title="Countries">
-                {continents.map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}
-                <Dropdown.Item href="#/action-1">z</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">z action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">z else</Dropdown.Item>
-                <DropdownButton id="dropdown-basic-button" title="Countries">
-                    {continents.map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}
+        <div align="center" split>
+            <DropdownButton id="dropdown-basic-button" title="Countries" split>
+                {continents.filter(x => x!=="Other").map(x => <><Dropdown.Item href="#/action-1">{x}</Dropdown.Item></>)}                
+                <DropdownButton id="dropdown-basic-button" title="Other">
+                    {continents.map(x => <><Dropdown.Item href="#/action-1">{x}(not supposed to be)</Dropdown.Item></>)}
 
                 </DropdownButton>
             </DropdownButton>
 
 
 
-            <Dropdown >
+            <Dropdown split>
                 <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
                 <Dropdown.Menu className="super-colors">
                     <Dropdown.Item eventKey="1">Action</Dropdown.Item>
@@ -55,9 +52,22 @@ const Dropdwn = () => {
                                 Active Item sdfsdlfhskjdhfkj
       </Dropdown.Item>
                             <Dropdown.Divider />
-                            <Dropdown >
+                            <Dropdown split>
                                 <Button variant="info">mix it up style-wise</Button>
                                 <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
+                                <Dropdown.Menu className="super-colors">
+                                    <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                                    <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                                    <Dropdown.Item eventKey="3" active>
+                                        Active Item
+      </Dropdown.Item>
+                                    <Dropdown.Divider />
+                                    <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            <Dropdown >
+                                <Button variant="info">mix it up style-wise</Button>
+                                <Dropdown.Toggle split variant="info" id="dropdown-custom-2" />
                                 <Dropdown.Menu className="super-colors">
                                     <Dropdown.Item eventKey="1">Action</Dropdown.Item>
                                     <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
