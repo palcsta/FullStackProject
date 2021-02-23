0ab32f67ea346ec4dfb199ce5e29a7dc81a60a9e
Your Name <you@example.com>
2021-02-23 12:39:56 +0200

    refactoring, filter functionality change

--- continents/src/components/Dropdwn.js
+++ continents/src/components/Dropdwn.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import Button from 'react-bootstrap/Button'
-import show from './Countries'
 import Countries from './Countries'
-//import Country from './Country'
-
-
+import Filter from './Filter'
+import axios from 'axios'
 
 
 const unique = (value, index, self) => {
@@ -14,75 +12,73 @@ const unique = (value, index, self) => {
 }
 
 
+const Dropdwn = () => {
+    const [filtered, setFiltered] = useState([])   
+    const [countries, setCountries] = useState([])
 
+const hook = () => {
+    console.log('effect countries')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+        console.log(countries)
+     
+      })
+  }
 
+  useEffect(hook, [])
 
-const Dropdwn = ({ data }) => {
-    const [filtered, setFiltered] = useState(data)
-    const [one, setOne] = useState(false)
+    const continents = countries.filter(x => x.region !== "Polar").map(x => x.region).filter(unique)
 
-
-
-    const continents = data.filter(x => x.region !== "Polar").map(x => x.region).filter(unique)
-
-    const polar = data.filter(x => x.region === "Polar").map(x => x.name)
-    const other = data.filter(x => x.region === "").map(x => x.name)
+    const polar = countries.filter(x => x.region === "Polar").map(x => x.name)
+    const other = countries.filter(x => x.region === "").map(x => x.name)
     //const polar = data.filter(x => x.region == "Americas")
 
     console.log('sub-continents in dropdown: ', polar)
     //console.log("data in dropdown: ", noContinent)
     return (<>
-        <div align="center" split>
+    <Filter countries={countries}/>
+        <div align="center" class="panel-footer " split>
             <DropdownButton id="dropdown-basic-button" title="Browse" split>
                 {continents.filter(x => x !== "").map(x => <>
-                    <Dropdown >
-                        <Button onClick={() => setFiltered(
-                            data.filter(r => r.region === x)
+                    <div align="center" class="panel-footer " split>
+                        <Dropdown >
+                            <Button onClick={() => setFiltered(
+                                countries.filter(r => r.region === x)
 
-                        )} variant="info">{x}</Button>
-                        <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
-                        <Dropdown.Menu className="super-colors">
+                            )} variant="info">{x}</Button>
+                            <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
+                            <Dropdown.Menu className="super-colors">
 
-                            {data.filter(y => y.region.includes(x)).map(x => x.subregion)
-                                .filter(unique).map(z =>
-                                    <>
-                                        <Dropdown >
-                                            <Button
-
-                                                onClick={() => setFiltered(
-                                                    data.filter(g => g.subregion === z)
-
-                                                )
-
-                                                }
-
-
-                                                variant="info">{"   " + z}</Button>
-                                            <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
-                                            <Dropdown.Menu className="super-colors">
-                                                {data.filter(a => a.subregion == z).map(b =>
-                                                    <><Dropdown.Item
-
-                                                        onClick={() => setFiltered(
-                                                            data.filter(q => q.name === b.name)
-
-                                                        )}
+                                {countries.filter(y => y.region.includes(x)).map(x => x.subregion)
+                                    .filter(unique).map(z =>
+                                        <>
+                                            <Dropdown >
+                                                <Button
+                                                    onClick={() => setFiltered(
+                                                        countries.filter(g => g.subregion === z))}
+                                                    variant="info">{"   " + z}</Button>
+                                                <Dropdown.Toggle split variant="success" id="dropdown-custom-2" />
+                                                <Dropdown.Menu className="super-colors">
+                                                    {countries.filter(a => a.subregion == z).map(b =>
+                                                        <><Dropdown.Item
+                                                            onClick={() => setFiltered(
+                                                                countries.filter(q => q.name === b.name)
+                                                            )}
+                                                        >{b.name}</Dropdown.Item></>)}
 
 
-                                                    >{b.name}</Dropdown.Item></>
-                                                )}
-
-
-                                            </Dropdown.Menu>
-                                        </Dropdown>
-                                        <Dropdown.Divider />
-                                    </>)}
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+                                            <Dropdown.Divider />
+                                        </>)}
 
 
 
 
-                        </Dropdown.Menu>
-                    </Dropdown>
+                            </Dropdown.Menu>
+                        </Dropdown></div>
                     <Dropdown.Divider />
 
 
@@ -92,11 +88,11 @@ const Dropdwn = ({ data }) => {
                 <Dropdown.Divider />
                 <DropdownButton variant="info" id="dropdown-basic-button" title="Other">
                     {other.map(x => <><Dropdown.Item onClick={() => setFiltered(
-                        data.filter(q => q.name === x)
+                        countries.filter(q => q.name === x)
                     )
                     }>{x}</Dropdown.Item></>)}
                     {polar.map(x => <><Dropdown.Item onClick={() => setFiltered(
-                        data.filter(q => q.name === x)
+                        countries.filter(q => q.name === x)
                     )
                     }>{x + "(Polar)"}</Dropdown.Item></>)}
                 </DropdownButton>
@@ -105,8 +101,7 @@ const Dropdwn = ({ data }) => {
 
 
 
-            <Countries countries={filtered}
-                one={one}
+            <Countries countries={filtered}               
             />
 
 
