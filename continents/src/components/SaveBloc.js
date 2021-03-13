6f69bc241845c9097ca01021bda17f5e4dd94c4d
zeeket <emil.heliste@helsinki.fi>
2021-03-13 21:34:45 +0200

    more blocsave form styles, tooltip

--- continents/src/components/SaveBloc.js
+++ continents/src/components/SaveBloc.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
 import { MdSave, MdLibraryAdd  } from 'react-icons/md'
 import { IconContext } from 'react-icons'
 import '../styles/SaveBloc.css'
@@ -52,18 +54,22 @@ const SaveBlocForm = (props) => {
             {
                 !showSaveBlocForm||!props.user?<>
                     <IconContext.Provider value={{ size: "1.25em", className: "saveButtonIcon" }}>
-                        <Button disabled={!props.user} onClick={()=>firstPress()}><MdLibraryAdd/>New Bloc</Button> 
+                        <OverlayTrigger overlay={<Tooltip id="tooltip-disabled" style={{display:props.user?"none":"inline"}}>You must be logged in to do this.</Tooltip>}>
+<span>
+                            <Button disabled={!props.user} style={{ pointerEvents: !props.user?'none':'auto' }} onClick={()=>firstPress()}><MdLibraryAdd/>New Bloc</Button> 
+</span>
+                            </OverlayTrigger>
                     </IconContext.Provider>
                     <p style={{color:"blue",display:blocSaved?"inline":"none"}}>Bloc Saved</p></>: 
                     <form onSubmit={pressSave}>
                         <div>
-                            <label>Name: </label>
+                            <label style={{marginRight:"0.2em"}}>Name: </label>
                             <input type="text" id="bloc-name" placeholder="Enter bloc name" onChange={handleBlocNameChange} />
                             <p style={{color:"red",display:blocSaveProblem.length?"inline":"none"}}>{blocSaveProblem}</p>
                         </div>
 
                         <IconContext.Provider value={{ size: "1.25em", className: "saveButtonIcon" }}>
-                            <Button variant="primary" type="submit"><MdSave/> Save</Button>
+                            <Button variant="primary" type="submit" style={{marginRight:"0.2em"}}><MdSave/> Save</Button>
                             <Button variant="secondary" onClick={()=>{pressCancel();setBlocSaveProblem("")}}>Cancel</Button>
                         </IconContext.Provider>
                     </form>
