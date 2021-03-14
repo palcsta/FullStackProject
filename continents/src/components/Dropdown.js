c298df101fa70358c7fb5f3f222d43857dfc97da
palcsta <you@example.com>
2021-03-14 19:25:40 +0200

    better visuality for dropdown items

--- continents/src/components/Dropdown.js
+++ continents/src/components/Dropdown.js
@@ -27,101 +27,106 @@ const CountriesDropdown = (props) => {
     <>
       <div align="center" className="panel-footer " split="true" className="mapTopButton">
         <DropdownButton id="dropdown-basic-button" title="Browse" split="true">
-          {continents.filter(x => x !== "").map(x => 
-          <>
-            <div key={x} align="center" className="panel-footer " split="true">
-              <Dropdown >
-                <Button onClick={() => {
-                  let matchingRegion = props.countries.filter(r => r.region === x)
-                  //console.log("selecting ",matchingRegion)
-                  let selection = []
-                  matchingRegion.forEach(match => selection.push(getA2(match)))
-                  //console.log("selecting alphacodes ",selection)
-                  props.selectMany(selection)
+          {continents.filter(x => x !== "").map(x =>
+            <div style={{ display: "inline block" }}>
+              <div key={x} align="center" className="panel-footer " split="true">
+                <Dropdown as={"ButtonGroup"}>
+                  <Button onClick={() => {
+                    let matchingRegion = props.countries.filter(r => r.region === x)
+                    //console.log("selecting ",matchingRegion)
+                    let selection = []
+                    matchingRegion.forEach(match => selection.push(getA2(match)))
+                    //console.log("selecting alphacodes ",selection)
+                    props.selectMany(selection)
                   }} variant="info" >{x}</Button>
-                <Dropdown.Toggle split="true" variant="success" id="dropdown-custom-2"/>
-                <Dropdown.Menu className="super-colors" >
+                  <Dropdown.Toggle split="true" variant="success" id="dropdown-custom-2" />
+                  <Dropdown.Menu className="super-colors" >
 
-                  {props.countries.filter(y => y.region.includes(x)).map(x => x.subregion)
+                    {props.countries.filter(y => y.region.includes(x)).map(x => x.subregion)
                       .filter(unique).map(z =>
-                        <>
-                          <Dropdown key={z}>
-                            <Button onClick={() => {
-                              let matchingSubregion = props.countries.filter(g => g.subregion === z)
-                              //console.log("selecting ", matchingSubregion)
-                              let selection = []
-                              matchingSubregion.forEach(match => selection.push(getA2(match)))
-                              //console.log("WHAT GIVING SETMANY ",selection)
-                              //console.log("selecting alphacodes ",selection)
-                              props.selectMany(selection)}} variant="info">{"   " + z}
-                            </Button>
-                            <Dropdown.Toggle split="true" variant="success" id="dropdown-custom-2" />
-                            <Dropdown.Menu className="super-colors">
-                              {props.countries.filter(a => a.subregion === z).map(b =>
-                              <>
-                                <Dropdown.Item 
-                                  key={b.name}
-                                  onClick={() => {
-                                    let matchingCountry = props.countries.find(q => q.name === b.name)
-                                    //console.log("selecting one country?",matchingCountry)
-                                     let  selection = getA2(matchingCountry)
-                                    //console.log("selecting alphacode ",selection)
-                                    props.selectOne(selection)
-                                    }
-                                  }>
-                                  {b.name}
-                                </Dropdown.Item></>)}
-                            </Dropdown.Menu>
+                        <div class=" " style={{ display: "inline block" }}>
+
+                          <Dropdown key={z} as={"ButtonGroup"}>
+                            <div style={{ display: "flex" ,margin:"5%"}}>
+                              <Button onClick={() => {
+                                let matchingSubregion = props.countries.filter(g => g.subregion === z)
+                                //console.log("selecting ", matchingSubregion)
+                                let selection = []
+                                matchingSubregion.forEach(match => selection.push(getA2(match)))
+                                //console.log("WHAT GIVING SETMANY ",selection)
+                                //console.log("selecting alphacodes ",selection)
+                                props.selectMany(selection)
+                              }} variant="info">{"   " + z}
+                              </Button>
+                              <Dropdown.Toggle split="true" variant="success" id="dropdown-custom-2" />
+                              <Dropdown.Menu className="super-colors">
+                                {props.countries.filter(a => a.subregion === z).map(b =>
+                                  <div style={{ display: "inline block" }}>
+                                    <Dropdown.Item
+                                      key={b.name}
+                                      onClick={() => {
+                                        let matchingCountry = props.countries.find(q => q.name === b.name)
+                                        //console.log("selecting one country?",matchingCountry)
+                                        let selection = getA2(matchingCountry)
+                                        //console.log("selecting alphacode ",selection)
+                                        props.selectOne(selection)
+                                      }
+                                      }>
+                                      {b.name}
+                                    </Dropdown.Item></div>)}
+                              </Dropdown.Menu>
+                            </div>
                           </Dropdown>
                           <Dropdown.Divider />
-                        </>
+                        </div>
                       )}
 
-                </Dropdown.Menu>
-              </Dropdown>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+              <Dropdown.Divider />
             </div>
-            <Dropdown.Divider />
-          </>
           )}
 
-<div align="center" className="panel-footer " split="true">
+          <div align="center" className="panel-footer " split="true">
 
-          <DropdownButton variant="info" id="dropdown-basic-button" title="Other">
-            {other.map(x => <><Dropdown.Item key={x} onClick={() => {
-              let matchingOther = props.countries.filter(q => q.name === x)
-              //console.log("selecting one from other ",matchingOther)
-              let selection = getA2(matchingOther[0])
-              props.selectOne(selection)
-            }}>{x}</Dropdown.Item></>)}
-            {polar.map(x => <><Dropdown.Item key={x} onClick={() => {
-              let matchingOther = props.countries.filter(q => q.name === x)
-              //console.log("selecting one from polar ",matchingOther)
-              let selection = getA2(matchingOther[0])
-              props.selectOne(selection)
-            }}>{x + "(Polar)"}</Dropdown.Item></>)}
-          </DropdownButton>
+            <DropdownButton variant="info" id="dropdown-basic-button" title="Other">
+              {other.map(x => <><Dropdown.Item key={x} onClick={() => {
+                let matchingOther = props.countries.filter(q => q.name === x)
+                //console.log("selecting one from other ",matchingOther)
+                let selection = getA2(matchingOther[0])
+                props.selectOne(selection)
+              }}>{x}</Dropdown.Item></>)}
+              {polar.map(x => <><Dropdown.Item key={x} onClick={() => {
+                let matchingOther = props.countries.filter(q => q.name === x)
+                //console.log("selecting one from polar ",matchingOther)
+                let selection = getA2(matchingOther[0])
+                props.selectOne(selection)
+              }}>{x + "(Polar)"}</Dropdown.Item></>)}
+            </DropdownButton>
             <Dropdown.Divider />
 
-  <OverlayTrigger overlay={<Tooltip id="tooltip-disabled" style={{display:(props.user && props.blocs.length)?"none":"inline"}}>{props.user?"You haven't saved any blocs yet.":"You must be logged in to do this."}</Tooltip>} placement="bottom">
-                          <span>
-          <DropdownButton disabled={!props.user||!props.blocs.length} style={{ pointerEvents: !props.user||!props.blocs.length?'none':'auto' }} variant="info" id="dropdown-basic-button" title="User's blocs">
-            {props.blocs.map(x => <><Dropdown.Item key={x.id} onClick={() => {
-              let matchingBloc = x.data.countries
-              //console.log("selecting ",matchingBloc)
-              if(matchingBloc.length>1){
-                props.selectMany(matchingBloc)
-              } else {
-                props.selectOne(matchingBloc[0])
-              }
-            }}>{x.data.name}</Dropdown.Item></>)}
-          </DropdownButton>
-                          </span>
-                            </OverlayTrigger>
+            <OverlayTrigger overlay={<Tooltip id="tooltip-disabled" style={{ display: (props.user && props.blocs.length) ? "none" : "inline" }}>{props.user ? "You haven't saved any blocs yet." : "You must be logged in to do this."}</Tooltip>} placement="bottom">
+              <span>
+                <DropdownButton disabled={!props.user || !props.blocs.length} style={{ pointerEvents: !props.user || !props.blocs.length ? 'none' : 'auto' }} variant="info" id="dropdown-basic-button" title="User's blocs">
+                  {props.blocs.map(x => <><Dropdown.Item key={x.id} onClick={() => {
+                    let matchingBloc = x.data.countries
+                    //console.log("selecting ",matchingBloc)
+                    if (matchingBloc.length > 1) {
+                      props.selectMany(matchingBloc)
+                    } else {
+                      props.selectOne(matchingBloc[0])
+                    }
+                  }}>{x.data.name}</Dropdown.Item></>)}
+                </DropdownButton>
+              </span>
+            </OverlayTrigger>
 
-</div>
+          </div>
         </DropdownButton>
       </div>
-    
-      </>
-  )}
+
+    </>
+  )
+}
 export default CountriesDropdown
